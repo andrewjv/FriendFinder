f15b79e24d78e96e222b83792a628e9e3bc89cfd
Andrew Villanueva <andrew@ANDREWs-MBP.hsd1.ca.comcast.net>
2018-05-24 16:45:45 -0700

    initial turn in

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+  {
+    'name': 'drew',
+    'photo': 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg',
+    'scores': [
+      1,
+      2,
+      3,
+      4,
+      5,
+      5,
+      4,
+      3,
+      2,
+      1,
+    ]
+  }
+];
+
+module.exports = friendsArray;
---
+++ routing/apiRoutes.js
@@ -0,0 +1,36 @@
+var friends = require('../app/data/friends');
+
+module.exports = function(app) {
+  
+  app.get('/api/friends', function(req,res) {
+    res.json(friends)
+  });
+
+  app.post('/api/friends', function(req, res) {
+    var newFriendScores = req.body.scores;
+    var scoresArray=[];
+    var friendCount = 0;
+    var bestMatch = 0;
+
+    for (var i = 0; i < friends.length; i++) {
+      
+      var scoresDiff = 0;
+
+      for (var j = 0; j < newFriendScores.length; j++) {
+        scoresDiff += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(newFriendScores[j])));
+      }
+
+      scoresArray.push(scoresDiff);
+    }
+
+    for(var i=0; i<scoresArray.length; i++){
+      if(scoresArray[i] <= scoresArray[bestMatch]){
+        bestMatch = i;
+      }
+    }
+    var theBestMatch = friends[bestMatch];
+    res.json(theBestMatch);
+
+    friends.push(req.body);
+  })
+}
