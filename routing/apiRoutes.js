72d15274c798a8823b9fc2e5acb1e5974659b042
Andrew Villanueva <andrew@ANDREWs-MBP.hsd1.ca.comcast.net>
2018-05-26 08:00:45 -0700

    revised comments.

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,33 +1,45 @@
+// linking route to friends data array
 var friends = require('../app/data/friends');
 
-module.exports = function(app) {
-  
-  app.get('/api/friends', function(req,res) {
+module.exports = function (app) {
+
+  // users are shown a JSON of the data in the table
+  app.get('/api/friends', function (req, res) {
     res.json(friends)
   });
 
-  app.post('/api/friends', function(req, res) {
+  // API POST request
+  // This code handles when a user submits a form and then submits the data to the server
+  app.post('/api/friends', function (req, res) {
+    
+    // variable for the new friend scores array
     var newFriendScores = req.body.scores;
-    var scoresArray=[];
+    var scoresArray = [];
     var friendCount = 0;
     var bestMatch = 0;
 
+    // for loop through the friends array
     for (var i = 0; i < friends.length; i++) {
-      
+
       var scoresDiff = 0;
 
+      // runs through the new friend scores array and compares them to the friends that are already in the array
       for (var j = 0; j < newFriendScores.length; j++) {
         scoresDiff += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(newFriendScores[j])));
       }
 
+      // pushes the result into the scoresArray
       scoresArray.push(scoresDiff);
     }
 
-    for(var i=0; i<scoresArray.length; i++){
-      if(scoresArray[i] <= scoresArray[bestMatch]){
+    // finds the best match
+    for (var i = 0; i < scoresArray.length; i++) {
+      if (scoresArray[i] <= scoresArray[bestMatch]) {
         bestMatch = i;
       }
     }
+
+    // returns the best match data
     var theBestMatch = friends[bestMatch];
     res.json(theBestMatch);
 
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,10 +1,15 @@
+// Path package to get the correct file path for html
 var path = require('path');
 
+
 module.exports = function(app) {
+
+  // get request for survey page
   app.get('/survey', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/survey.html'));
   });
 
+  // if no matching route is found it will default to the home page
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/home.html'));
   });
